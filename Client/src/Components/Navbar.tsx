89f2713f98dfe065ec25eea1c50083763059e32f
ArunavaCoderEm <acecankill@gmail.com>
2024-06-01 23:22:32 +0530

    push home page ready !!!!

--- Client/src/Components/Navbar.tsx
+++ Client/src/Components/Navbar.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-
-export default function Navbar(): React.ReactNode {
-  const [isSearchOpen, setIsSearchOpen] = useState<boolean>(false);
-  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-  const [menu,setMenu] = useState<string>('home');
+export default function Navbar() {
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [menu, setMenu] = useState('home');
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -13,52 +12,61 @@ export default function Navbar(): React.ReactNode {
   };
 
   return (
-    <>
-      <nav className="bg-white border-gray-200 dark:bg-gray-800">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse" onClick={() => {setMenu('home')}}>
-            <img src="./logonr.jpg" className="h-8 rounded-full"/>
-            <span className="self-center text-xl font-mono font-semibold whitespace-nowrap hov dark:text-white"><span className='text-blue-300 font-serif font-bold'>N</span>umber<span className='text-blue-300 font-serif font-bold'>R</span>oulette</span>
-          </Link>
-          <div className="flex md:order-2">
-            
-            <button type="button" onClick={toggleMenu} className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
-              <span className="sr-only">Open main menu</span>
-              <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
-              </svg>
-            </button>
-          </div>
-          <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isSearchOpen ? 'block' : 'hidden'}`} id="navbar-search">
-            <div className="relative mt-3 md:hidden">
-              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                </svg>
-              </div>
-            </div>
-          </div>
-          <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isMenuOpen ? 'block' : 'hidden'}`}>
-            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 text-center md:bg-white dark:bg-gray-600 md:dark:bg-gray-800 dark:border-gray-700">
-              <li>
-                <Link to="/" className={`block p-2 font-sm rounded-md hover:text-slate-500 transition-all duration-300 ${(menu == 'home'? `bg-slate-500 text-black` : `text-white`)}`} onClick={() => {setMenu('home')}} aria-current="page">Home</Link>
-              </li>
-              <li>
-                <Link to="/about"  className={`block p-2 font-sm rounded-md hover:text-slate-500 transition-all duration-300 ${(menu == 'about'? `bg-slate-500 text-black` : `text-white`)}`} onClick={() => {setMenu('about')}}>About Me</Link>
-              </li>
-              <li>
-                <Link to="/features"  className={`block p-2 font-sm rounded-md hover:text-slate-500 transition-all duration-300 ${(menu == 'feature'? `bg-slate-500 text-black` : `text-white`)}`} onClick={() => {setMenu('feature')}}>Features</Link>
-              </li>
-              <li>
-              </li>
-            </ul>
-            <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isSearchOpen ? 'block' : 'hidden'}`} id="navbar-search">
-            <div className="relative mx-auto md:block sm:hiddden lg:block">
-            </div>
-          </div>
-          </div>
+    <nav className="bg-white border-gray-200 dark:bg-gray-800">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse" onClick={() => setMenu('home')}>
+          <img src="./logonr.jpg" alt="logo" className="h-8 rounded-full" />
+          <span className="self-center text-xl font-mono font-semibold whitespace-nowrap hover:text-blue-500 dark:text-white">
+            <span className="text-blue-300 font-serif font-bold">N</span>umber<span className="text-blue-300 font-serif font-bold">R</span>oulette
+          </span>
+        </Link>
+        <div className="flex md:order-2">
+          <button
+            type="button"
+            onClick={toggleMenu}
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-search"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M3 12h18M3 6h18M3 18h18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
+          </button>
         </div>
-      </nav>
-    </>
+        <div className={`w-full md:flex md:w-auto md:order-1 ${isMenuOpen ? 'block' : 'hidden'}`}>
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 text-center md:bg-white dark:bg-gray-600 md:dark:bg-gray-800 dark:border-gray-700">
+            <li>
+              <Link
+                to="/"
+                className={`block p-2 font-sm rounded-md hover:text-slate-500 transition-all duration-300 ${menu === 'home' ? 'bg-slate-500 text-black' : 'text-white'}`}
+                onClick={() => setMenu('home')}
+                aria-current="page"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/about"
+                className={`block p-2 font-sm rounded-md hover:text-slate-500 transition-all duration-300 ${menu === 'about' ? 'bg-slate-500 text-black' : 'text-white'}`}
+                onClick={() => setMenu('about')}
+              >
+                Rules
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/features"
+                className={`block p-2 font-sm rounded-md hover:text-slate-500 transition-all duration-300 ${menu === 'feature' ? 'bg-slate-500 text-black' : 'text-white'}`}
+                onClick={() => setMenu('feature')}
+              >
+                Features
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
--- Client/src/Pages/Home.tsx
+++ Client/src/Pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import TextTicker from "../Components/TextTicker"
 
 export default function Home():React.ReactNode {
 
@@ -24,14 +25,22 @@ export default function Home():React.ReactNode {
     <>
     <div className="my-5">
       <h1
-      className="bg-clip-text font-extrabold text-center text-3xl my-5 text-transparent bg-gradient-to-br from-blue-400 to-blue-800">
+      className="bg-clip-text font-extrabold text-center text-4xl my-5 text-transparent bg-gradient-to-br from-blue-400 to-blue-800">
         Welcome to #1 Number Roulette Game</h1>
-      <div className="grid lg:grid-cols-2 sm:grid-cols-1 h-screen">
-          <div className="flex flex-col align-middle items-center justify-center">
-            <h2 className="text-center mt-10 mb-4 font-bold bg-clip-text text-transparent bg-gradient-to-br from-slate-200 to-slate-400 text-2xl">Play as a guest && Test Your Luck !!</h2>
-            <button onClick={handleConnect} className="p-4 bg-gradient-to-b from-green-700 to-green-900 rounded-md font-semibold m-3 text-white hover:scale-110 transition-all duration-300 hover:text-slate-300">Join Game Room</button>
+        <img src="./rou.jpg" className="absolute z-[-1] sha bg-cover opacity-30 w-[900px] h-[500px] left-0 right-0 mx-auto" alt="imagebg" />
+
+          <div className="flex flex-col items-center align-middle justify-center mt-20">
+            <div className="backdrop-blur-sm rounded-md sha bg-blue-600/40">
+          <div className="text-3xl text-center font-semibold tabular-nums tracking-tight text-blue-200">
+            <h2 className="bg-clip-text font-extrabold text-center text-3xl my-5 text-transparent bg-gradient-to-br from-slate-300 via-slate-400 to-slate-300">Our Trusted Users</h2>
+            <TextTicker value={690} /> 
           </div>
-      </div>
+            <h2 className="text-center mt-5 mb-4 font-bold bg-clip-text text-transparent bg-gradient-to-br mx-1 from-slate-200 to-slate-400 text-3xl p-5">Play multiplayer as a guest & Test<br/> Your Winning Luck !!</h2>
+            <div className="flex justify-center">
+            <button onClick={handleConnect} className="p-4 m-3 bg-gradient-to-b from-green-700 to-green-900 rounded-md font-semibold text-white hover:scale-110 transition-all duration-300 hover:text-slate-300">Join Game Room</button>
+            </div>
+          </div>
+          </div>
       </div>
     </>
   )
