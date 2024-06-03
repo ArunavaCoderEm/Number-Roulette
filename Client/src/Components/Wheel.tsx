20173c334d470d4f35abec89e136c41989681640
ArunavaCoderEm <acecankill@gmail.com>
2024-06-03 20:44:32 +0530

    pushes !! and damn I am good

--- Client/src/Components/Wheel.tsx
+++ Client/src/Components/Wheel.tsx
@@ -1,37 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import { useWebSocket } from "../Websocket/WebSocketProvider"
 
-const NumberWheel: React.FC = () => {
+
+const NumberWheel: React.FC<{ onResultChange: (
+ resulto: number, scoreo: number, resultt: number, scoret: number, winner: String | null) => void }> = ({ onResultChange }) => {
   const [spinning, setSpinning] = useState(false);
-  const [result, setResult] = useState<number | null>(0);
-  const [score, setscore] = useState<number | null>(0);
+  const [mynum, setmynum] = useState<number>(0)
+  const [notwin, setnotwin] = useState<boolean | null>(true)
 
   const socket = useWebSocket();
 
-
   useEffect(() => {
     if (!socket) return;
+
     socket.onmessage = (e: any) => {
       const message = JSON.parse(e.data)
       console.log(message)
-    }             
+      onResultChange(message.player1Num, message.player1Score ,message.player2Num, message.player2Score, message.winner);
+      if(message.winner){
+        setnotwin(false)
+      }
+    };
   }, [socket]);
-
 
   const startSpinning = () => {
     setSpinning(true);
-    const randomResult = Math.floor(Math.random() * (20 - 10 + 1));
     setTimeout(() => {
       setSpinning(false);
-      setResult(randomResult);
     }, 3000);
+    const randomResult = Math.floor(Math.random() * (20 - 10 + 1)); 
     if (socket && socket.readyState === WebSocket.OPEN) {
       socket.send(
         JSON.stringify({
           type: 'choose',
-          plnum: result
+          plnum: randomResult 
         })
       );
+      setmynum(randomResult)
     } else {
       console.error('WebSocket is not open');
     }
@@ -42,14 +47,15 @@ const NumberWheel: React.FC = () => {
       <h2 className='text-2xl text-center text-white mt-10 mb-5'>Number Wheel</h2>
       <div className={`wheel ${spinning ? 'spin' : ''}`}>
         <div className='flex justify-center my-5 mx-auto'>
-        <img src="./spin.png" className='img2' alt="Number Wheel" />
+          <img src="./spin.png" className='img2' alt="Number Wheel" />
         </div>
       </div>
-        <h1 className='text-white text-center font-semibold text-xl'>Your Number - {result}</h1>
       <div className="flex justify-center">
-        <button onClick={startSpinning} className="p-4 sha mt-5 bg-gradient-to-b from-green-700 to-green-900 rounded-md font-semibold text-white hover:scale-110 transition-all duration-300 hover:text-slate-300">Spin Wheel</button>
-    </div>
-    <h1 className='text-white my-5 text-center font-semibold text-xl'>Score - {score}</h1>
+        {notwin &&
+        <button onClick={startSpinning} className="p-4 sha mt-5 mb-3 bg-gradient-to-b from-green-700 to-green-900 rounded-md font-semibold text-white hover:scale-110 transition-all duration-300 hover:text-slate-300">Spin Wheel</button>
+        }
+      </div>
+        <h1 className='text-white text-center font-semibold text-xl my-10'>My number - {mynum}</h1>
     </div>
   );
 };
--- Client/src/Pages/Gameroom.tsx
+++ Client/src/Pages/Gameroom.tsx
@@ -4,9 +4,27 @@ import BounceLoader from "../Components/Loader";
 import { useLocation } from 'react-router-dom';
 
 export default function Gameroom(): React.ReactNode {
+  const [resultTwo, setResultTwo] = useState<number | null>(0);
+  const [scoreTwo, setScoreTwo] = useState<number | null>(0);
+  const [resultOne, setResultOne] = useState<number | null>(0);
+  const [scoreOne, setScoreOne] = useState<number | null>(0);
+  const [winner, setwinner] = useState<String | null>(null);
   const [load, setLoad] = useState<boolean>(true);
   const [play, setPlay] = useState<String | null>("Player");
   const location = useLocation();
+
+  const handleResultChange = (resulto: number, scoreo: number, resultt: number, scoret: number, winner: String | null) => {
+    setResultTwo(resultt);
+    setScoreTwo(scoret);
+    setResultOne(resulto);
+    setScoreOne(scoreo);
+    if(winner) {
+      setTimeout(() => {
+        setwinner(winner)
+      }, 1000);
+    }
+  };
+
 
   useEffect(() => {
     const mes = new URLSearchParams(location.search).get("message");
@@ -23,20 +41,44 @@ export default function Gameroom(): React.ReactNode {
           <BounceLoader/>
         </div>
       }
-      {!load && 
+      {!load && !winner &&
         <>
-          <div className='grid lg:grid-cols-2 mt-5 sm:grid-cols-1 divide-x-*'>
-            <div className='lg:border-r-2 h-screen flex flex-col items-center'>
-              <h1 className='text-white text-center font-thin text-3xl'>{play}</h1>
-              <Wheel/>
-            </div>
-            <div className='h-screen flex flex-col items-center'>
-              <h1 className='text-white text-center font-thin text-3xl'>Opponent</h1>
-              <img src="./player.jpg" alt="player" className='w-32 my-14' />
-              <h1 className='text-white text-center font-semibold text-xl my-10'>Opponent number - </h1>
-              <h1 className='text-white text-center font-semibold text-xl'>Opponent score - </h1>
-            </div>
-          </div>
+        {play == "player1" ? 
+        
+        <div className='grid lg:grid-cols-2 mt-5 sm:grid-cols-1 divide-x-*'>
+        <div className='lg:border-r-2 h-screen flex flex-col items-center'>
+          <h1 className='text-white text-center font-thin text-3xl'>{play}</h1>
+          <Wheel onResultChange={handleResultChange}/>
+          <h1 className='text-white text-center font-semibold text-xl'>My score - {scoreOne}</h1>
+        </div>
+        <div className='h-screen flex flex-col items-center'>
+          <h1 className='text-white text-center font-thin text-3xl'>Opponent</h1>
+          <img src="./player.jpg" alt="player" className='w-32 my-14' />
+          <h1 className='text-white text-center font-semibold text-xl my-10'>Opponent number - {resultTwo}</h1>
+          <h1 className='text-white text-center font-semibold text-xl'>Opponent score - {scoreTwo}</h1>
+        </div>
+      </div>    
+        
+        :
+
+        <div className='grid lg:grid-cols-2 mt-5 sm:grid-cols-1 divide-x-*'>
+        <div className='lg:border-r-2 h-screen flex flex-col items-center'>
+          <h1 className='text-white text-center font-thin text-3xl'>{play}</h1>
+          <Wheel onResultChange={handleResultChange}/>
+          <h1 className='text-white text-center font-semibold text-xl'>My score - {scoreTwo}</h1>
+        </div>
+        <div className='h-screen flex flex-col items-center'>
+          <h1 className='text-white text-center font-thin text-3xl'>Opponent</h1>
+          <img src="./player.jpg" alt="player" className='w-32 my-14' />
+          <h1 className='text-white text-center font-semibold text-xl my-10'>Opponent number - {resultOne}</h1>
+          <h1 className='text-white text-center font-semibold text-xl'>Opponent score - {scoreOne}</h1>
+        </div>
+      </div>    
+        
+        }
+      {winner && 
+        <h1 className='text-center text-white'>Winner is {winner}</h1>
+      }
         </>
       }
     </>
